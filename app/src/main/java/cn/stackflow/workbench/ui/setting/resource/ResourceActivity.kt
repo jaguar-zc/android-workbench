9978918e53caaa53a76efea1357a5f242cc31580
jaguar_zc@sina.com <jaguar_zc@sina.com>
2020-08-14 22:58:17 +0800

    22

--- app/src/main/java/cn/stackflow/workbench/ui/setting/resource/ResourceActivity.kt
+++ app/src/main/java/cn/stackflow/workbench/ui/setting/resource/ResourceActivity.kt
@@ -2,45 +2,81 @@ package cn.stackflow.workbench.ui.setting.resource
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.Observer
 import cn.stackflow.workbench.R
-import cn.stackflow.workbench.common.base.ListActivity
-import cn.stackflow.workbench.common.bean.DeptDTO
+import cn.stackflow.workbench.common.base.BaseActivity
 import cn.stackflow.workbench.common.bean.ResourceDTO
-import cn.stackflow.workbench.common.bean.RoleDTO
-import cn.stackflow.workbench.ui.adapter.BaseBindingAdapter
+import cn.stackflow.workbench.databinding.ResourceActivityBinding
+import com.orhanobut.logger.Logger
+import com.unnamed.b.atv.model.TreeNode
+import com.unnamed.b.atv.view.AndroidTreeView
+import kotlinx.android.synthetic.main.resource_activity.*
 import kotlinx.android.synthetic.main.toolbar.*
 import kotlinx.android.synthetic.main.toolbar.view.*
+
 
 /**
  * @author <a href="mailto:jenly1314@gmail.com">Jenly</a>
  */
-class ResourceActivity : ListActivity<ResourceDTO, ResourceViewModel>() {
+class ResourceActivity : BaseActivity<ResourceViewModel, ResourceActivityBinding>() {
 
-    override fun createAdapter(): BaseBindingAdapter<ResourceDTO> {
-        return BaseBindingAdapter(R.layout.resource_item)
-    }
 
     override fun initData(savedInstanceState: Bundle?) {
         super.initData(savedInstanceState)
         setToolbarTitle("资源管理")
         toolbar.ivRight.setImageResource(R.drawable.push)
+//        val root = TreeNode.root()
+//        val parent = TreeNode("MyParentNode")
+//        val child0 = TreeNode("ChildNode0")
+//        val child1 = TreeNode("ChildNode1")
+//        parent.addChildren(child0, child1)
+//        root.addChild(parent)
+//        val tView = AndroidTreeView(context, root)
+//        srl.addView(tView.view)
+
+        viewModel.liveData.observe(this,Observer {
+            refreshTreeView(it)
+        })
+
+
+        viewModel.requestData(1)
+
+
+
+//        val nodeItem = IconTreeItem()
+//        val child1 =  TreeNode(nodeItem).setViewHolder(ResourceHolder(context))
+
     }
+
+
+
+
+
+    private fun createChildrenNote(resourceDTO: ResourceDTO): TreeNode{
+        return  TreeNode(resourceDTO).setViewHolder(ResourceHolder(context,llView));
+    }
+
+
+    private fun refreshTreeView(list: List<ResourceDTO>){
+        val root = TreeNode.root()
+        list.forEach { it ->
+            val parent = TreeNode(it).setViewHolder(ResourceHolder(context,llView))
+            it.children.forEach { ii ->
+                run {
+                    parent.addChild(createChildrenNote(ii))
+                }
+            }
+            root?.addChild(parent)
+        }
+        val tView = AndroidTreeView(this, root)
+        llView.addView(tView.view)
+    }
+
 
     override fun getLayoutId(): Int {
         return R.layout.resource_activity
     }
 
-    override fun clickItem(data: ResourceDTO) {
-        data.key?.let {
-//            showToast(it)
-//            var intent = Intent(this,
-//                DeptEditActivity::class.java)
-//            intent.putExtra(Constants.KEY_ID,data.id)
-//            intent.putExtra(Constants.KEY_TITLE,data.name)
-//            intent.putExtra(Constants.KEY_CONTENT,data.remark)
-//            startActivity(intent)
-        }
-    }
 
     override fun onClick(v: View) {
         super.onClick(v)
@@ -49,11 +85,6 @@ class ResourceActivity : ListActivity<ResourceDTO, ResourceViewModel>() {
 //                DeptEditActivity::class.java)
 //            startActivity(intent)
         }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        requestData(1)
     }
 
 }
---
+++ app/src/main/java/cn/stackflow/workbench/ui/setting/resource/ResourceHolder.java
@@ -0,0 +1,42 @@
+package cn.stackflow.workbench.ui.setting.resource;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.unnamed.b.atv.model.TreeNode;
+
+import cn.stackflow.workbench.R;
+import cn.stackflow.workbench.common.bean.ResourceDTO;
+
+public class ResourceHolder extends TreeNode.BaseNodeViewHolder<ResourceDTO> {
+    LinearLayout linearLayout;
+    public ResourceHolder(Context context) {
+        super(context);
+    }
+
+    public ResourceHolder(Context context, LinearLayout linearLayout) {
+        super(context);
+        this.linearLayout = linearLayout;
+    }
+
+    @Override
+    public View createNodeView(TreeNode node, ResourceDTO value) {
+        final LayoutInflater inflater = LayoutInflater.from(context);
+        final View view = inflater.inflate(R.layout.resource_node,  linearLayout, false);
+        TextView tvValue = (TextView) view.findViewById(R.id.tvIcon);
+        ImageView ivJump = (ImageView) view.findViewById(R.id.ivJump);
+        tvValue.setText(value.getTitle());
+        if(value.getChildren() != null && value.getChildren().size() > 0){
+            ivJump.setVisibility(View.VISIBLE);
+        }else{
+            ivJump.setVisibility(View.INVISIBLE);
+        }
+        return view;
+    }
+
+}
